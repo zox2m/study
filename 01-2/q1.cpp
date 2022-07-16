32a1745ddecf28c6cb5c6347f4198ecc611d99de
zox2m <jumin0913@naver.com>
2022-07-16 13:31:45 +0900

    01-2

---
+++ 01-2/q1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+void swap(int *a,int *b){
+    int *temp;
+    temp = a;
+    a = b;
+    b = temp;    
+}
+
+void swap(char *a,char *b){
+    char *temp;
+    temp = a;
+    a = b;
+    b = temp;    
+}
+
+void swap(double *a,double *b){
+    double *temp;
+    temp = a;
+    a = b;
+    b = temp;    
+}
+
+int main(void)
+{
+    int num1=20, num2=30;
+    swap(&num1,&num2);
+    std::cout<<num1<<' '<<num2<<std::endl;
+
+    char ch1='A',ch2='Z';
+    swap(&ch1,&ch2);
+    std::cout<<ch1<<' '<<ch2<<std::endl;
+
+    double db1=1.111, db2=5.555;
+    swap(&db1,&db2);
+    std::cout<<db1<<' '<<db2<<std::endl;
+
+    return 0;
+}
