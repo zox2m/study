77661ddccc82e1188b779f7a8caea91c41eed87e
zox2m <zox2mm@gmail.com>
2024-03-07 16:25:30 +0900

    1주차 목 객프

---
+++ 24cpp/Chap2/lab6/sizeOfType.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "short int의 크기는" << sizeof(short int) << "바이트입니다" << endl;
+	cout << "int의 크기는" << sizeof(int) << "바이트입니다" << endl;
+
+	cout << "long int의 크기는" << sizeof(long int) << "바이트입니다" << endl;
+
+	return 0;
+}
