8c4ffb8879956f4da8b89eb9962e60b27e705618
zox2m <zox2mm@gmail.com>
2024-04-05 18:56:11 +0900

    coding test

---
+++ codingTest/1000.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {    
+    long long num1,num2,num3;
+    //cout << "enter two num .. :" ;
+    // 아니 ㅋㅋㅋㅋ이줄 안빼서 계속 오류나고있었네 ㅋㅋㅋ ;;;;
+    cin >> num1 >>num2;
+    num3 = num1 + num2;
+    cout << num3;
+    return 0;
+}
