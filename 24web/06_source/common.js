7bb2856e8d93747c55e05c38f1cb321af73a379c
zox2m <zox2mm@gmail.com>
2024-05-15 19:21:31 +0900

    web programming

---
+++ 24web/06_source/common.js
@@ -0,0 +1,12 @@
+// 로그인 가능한 계정 (아이디, 패드워드) 선언
+function getLogin() {
+
+}
+
+
+// main.html 에서 입력 받은 id 및 password 를 변수에 저장
+var id = document.querySelector('#id');
+var pw = document.querySelector('#password');
+
+
+// id 및 password check 조건문 작성
---
+++ 24web/openAPI.py
@@ -0,0 +1,6 @@
+import requests
+import pprint
+import json
+
+url = "apis.data.go.kr/5090000/goodRestaurantStatusService?serviceKey=fC5An%2BEN3x%2F1TJZyWrX9IB1%2BPDtQxZeFu9LwWWX9S%2FpQSjZPkpR5e8DEz%2FBUfEEE3o%2BCSmJmJt8VXpG3sKN9Og%3D%3D&pageNo=1&numOfRows=1000&type=json"
+
